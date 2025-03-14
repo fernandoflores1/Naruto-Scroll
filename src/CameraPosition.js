c36ea7f595beb330e6625607634597a718c619e0
fernandoflores1 <115038723+fernandoflores1@users.noreply.github.com>
2025-03-14 04:09:55 +0100

    Initial commit

---
+++ src/CameraPosition.js
@@ -0,0 +1,7 @@
+export const CAMERA_POSITIONS = [
+  { x: 150, y: 300, z: 800, rotationX: 0, rotationY: 0, rotationZ: 0 }, // Vista frontal ajustada
+  { x: 800, y: 200, z: 700, rotationX: 0, rotationY: 1, rotationZ: 0 }, // Vista trasera mejorada
+  { x: 200, y: 600, z: 200, rotationX: -1.5, rotationY: 0, rotationZ: 0 }, // Vista superior corregida
+  { x: 0, y: 250, z: -1000, rotationX: 0, rotationY: 3.14, rotationZ: 0 }, // Vista trasera con mejor profundidad
+  { x: 0, y: 600, z: -600, rotationX: 0.25, rotationY: 3.14, rotationZ: 0 }, // Vista lateral con mejor distancia
+];
